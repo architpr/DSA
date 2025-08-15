0c5431737ec595bb1d316710e14485469e578449
architpr <157139242+architpr@users.noreply.github.com>
2025-08-15 15:12:24 +0530

    Time: 355 ms (8.02%) | Memory: 16.8 MB (81.81%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        if (k < 0) return 0; // difference cannot be negative
+        int n = nums.size();
+        set <pair<int ,int >> uniquepairs;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+               if (abs(nums[i]-nums[j])==k ){
+                int a = min(nums[i],nums[j]);
+                int b = max(nums[i],nums[j]);
+                uniquepairs.insert({a,b});
+               }
 
-        unordered_map<int, int> freq;
-        for (int num : nums) {
-            freq[num]++;
-        }
 
-        int count = 0;
-        for (auto &p : freq) {
-            if (k == 0) {
-                // count numbers that appear more than once
-                if (p.second > 1) count++;
-            } else {
-                // check if num + k exists
-                if (freq.count(p.first + k)) count++;
+
+                // int diff = abs(nums[i] - nums[j]);
+                // if (diff == k) {
+                //     count++;
+                }
             }
+            return uniquepairs.size();
         }
-        return count;
-    }
+
+        
+    
+    
 };
